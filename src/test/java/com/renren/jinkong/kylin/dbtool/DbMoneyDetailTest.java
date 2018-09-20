fbc40484bd2704ae8a572db4d5e55528a54b47f9
tianlixin <newcitysoft@163.com>
2018-09-20 17:20:34 +0800

    修改测试用例。

---
+++ src/test/java/com/renren/jinkong/kylin/dbtool/DbMoneyDetailTest.java
@@ -0,0 +1,108 @@
+package com.renren.jinkong.kylin.dbtool;
+
+import com.renren.jinkong.kylin.dbtool.core.executor.DefaultDataSourceExecutor;
+import com.renren.jinkong.kylin.dbtool.excel.ExcelKit;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.junit.Test;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author lixin.tian@renren-inc.com
+ * @date 2018/9/20 13:24
+ */
+public class DbMoneyDetailTest {
+
+    private static final String url = "jdbc:mysql://localhost:3306/springboot_demo?characterEncoding=utf8&zeroDateTimeBehavior=convertToNull";
+    private static final String user = "root";
+    private static final String password = "root";
+
+    @Test
+    public void testAdd() throws Exception {
+        File file = new File("D:\\data\\01.xls");
+        ExcelKit excelKit = new ExcelKit(file, MoneyDetail.class);
+        DefaultDataSourceExecutor executor = new DefaultDataSourceExecutor(url, user, password);
+
+        excelKit.setSheet("Sheet1");
+
+        List list = excelKit.dataExcelMapToBean();
+        int i = executor.batchInsert(MoneyDetail.class, list);
+        System.out.println(i);
+    }
+
+    @Test
+    public void testAdd2() throws Exception {
+        File file = new File("D:\\data\\车商资本金明细.xls");
+        ExcelKit excelKit = new ExcelKit(file, MoneyDetail.class);
+        DefaultDataSourceExecutor executor = new DefaultDataSourceExecutor(url, user, password);
+
+        List list = excelKit.dataExcelMapToBean(1);
+        int i = executor.batchInsert(MoneyDetail.class, list);
+        System.out.println(i);
+    }
+
+    @Test
+    public void testSheet() throws Exception {
+        File file = new File("D:\\data\\车商资本金明细.xls");
+        ExcelKit excelKit = new ExcelKit(file, MoneyDetail.class);
+
+        excelKit.setSheet("车商资本金明细");
+
+
+        Sheet sheet = excelKit.getSheet();
+        System.out.println(sheet);
+    }
+
+    @Test
+    public void testHss() throws Exception {
+        File file = new File("D:\\data\\车商资本金明细.xls");
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+        HSSFWorkbook workbook = new HSSFWorkbook(bis);
+
+        int numberOfSheets = workbook.getNumberOfSheets();
+
+        for (int i = 0; i < numberOfSheets; i++) {
+            String sheetName = workbook.getSheetName(i);
+            System.out.println(sheetName);
+            System.out.println("表头：" + getHeadMap(workbook.getSheetAt(i), 0));
+        }
+
+        System.out.println(numberOfSheets);
+    }
+
+    @Test
+    public void testHss2() throws Exception {
+        File file = new File("D:\\data\\车商资本金明细.xls");
+        BufferedInputStream bis = new BufferedInputStream(new FileInputStream(file));
+        HSSFWorkbook workbook = new HSSFWorkbook(bis);
+
+        int activeSheetIndex = workbook.getActiveSheetIndex();
+        HSSFSheet sheetAt = workbook.getSheetAt(activeSheetIndex);
+
+        System.out.println(sheetAt.getSheetName());
+        System.out.println("表头：" + getHeadMap(sheetAt, 1));
+    }
+
+    private Map<String, Integer> getHeadMap(Sheet sheet, int row) {
+        Map<String, Integer> map = new HashMap<>();
+
+        Row root = sheet.getRow(row);
+
+        short lastCellNum = root.getLastCellNum();
+
+        for(int i = 0; i < lastCellNum; i++) {
+            map.put(ExcelKit.getCellValue(root.getCell(i)), i);
+        }
+
+        return map;
+    }
+
+}
---
+++ src/test/java/com/renren/jinkong/kylin/dbtool/MoneyDetail.java
@@ -0,0 +1,124 @@
+package com.renren.jinkong.kylin.dbtool;
+
+import com.renren.jinkong.kylin.dbtool.anno.GeneratedValue;
+import com.renren.jinkong.kylin.dbtool.anno.Id;
+import com.renren.jinkong.kylin.dbtool.anno.Mapping;
+import com.renren.jinkong.kylin.dbtool.anno.NotBlank;
+import com.renren.jinkong.kylin.dbtool.anno.Table;
+
+import java.util.Date;
+
+@Table(name = "tb_money_detail")
+public class MoneyDetail {
+
+    /**
+     * 自增主键
+     */
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    /**
+     * JV公司全称
+     */
+    @NotBlank
+    @Mapping(cellName = "JV公司全称")
+    private String fullName;
+
+    /**
+     * 股东性质
+     */
+    @Mapping(cellName = "股东性质")
+    private String nature;
+
+    /**
+     * 名称
+     */
+    @Mapping(cellName = "名称")
+    private String name;
+
+    /**
+     * 认缴金额
+     */
+    @Mapping(cellName = "认缴金额")
+    private Double money;
+
+    /**
+     * 认缴时间
+     */
+    @Mapping(cellName = "认缴时间")
+    private Date date;
+
+    /**
+     * 实缴金额
+     */
+    @Mapping(cellName = "实缴金额")
+    private Double money2;
+
+    /**
+     * 出资形式
+     */
+    @Mapping(cellName = "出资形式")
+    private String form;
+
+    /**
+     * 出资账户名
+     */
+    @Mapping(cellName = "出资账户名")
+    private String accountName;
+
+    /**
+     * 出资账号
+     */
+    @Mapping(cellName = "出资账号")
+    private String account;
+
+    /**
+     * 出资开户行
+     */
+    @Mapping(cellName = "出资开户行")
+    private String accountBank;
+
+    /**
+     * 收款账户名
+     */
+    @Mapping(cellName = "收款账户名")
+    private String acceptAccountName;
+
+    /**
+     * 收款账号
+     */
+    @Mapping(cellName = "收款账号")
+    private String acceptAccount;
+
+    /**
+     * 收款开户行
+     */
+    @Mapping(cellName = "收款开户行")
+    private String acceptAccountBank;
+
+    /**
+     * 增资金额
+     */
+    @Mapping(cellName = "增资金额")
+    private Double addMoney;
+
+    /**
+     * 撤资时间
+     */
+    @Mapping(cellName = "撤资时间")
+    private Double releaseMoney;
+
+    /**
+     * 撤资金额
+     */
+    @Mapping(cellName = "撤资金额")
+    private Date releaseMoneyDate;
+
+    /**
+     * 工商注册资本金
+     */
+    @Mapping(cellName = "工商注册资本金")
+    private Double registerMoney;
+
+}

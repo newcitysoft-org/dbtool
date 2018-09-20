364f8230059cd70527bf821a3d0e67f90b1041ef
tianlixin <newcitysoft@163.com>
2018-09-20 14:07:05 +0800

    增加映射注解。

---
+++ src/main/java/com/renren/jinkong/kylin/dbtool/anno/Mapping.java
@@ -0,0 +1,19 @@
+package com.renren.jinkong.kylin.dbtool.anno;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+/**
+ * 字段映射到值
+ *
+ * @author lixin.tian@renren-inc.com
+ * @date 2018/9/20 11:38
+ */
+@Target(FIELD)
+@Retention(RUNTIME)
+public @interface Mapping {
+    String cellName();
+}

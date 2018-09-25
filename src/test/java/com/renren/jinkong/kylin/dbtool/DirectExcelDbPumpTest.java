9fe2b668f8095cb8e9e736f2be90a1c8dc2146b4
tianlixin <newcitysoft@163.com>
2018-09-25 17:08:44 +0800

    注释测试类。

--- src/test/java/com/renren/jinkong/kylin/dbtool/DirectExcelDbPumpTest.java
+++ src/test/java/com/renren/jinkong/kylin/dbtool/DirectExcelDbPumpTest.java
@@ -1,35 +1,35 @@
-package com.renren.jinkong.kylin.dbtool;
-
-import com.renren.jinkong.kylin.dbtool.core.op.DbInMode;
-import com.renren.jinkong.kylin.dbtool.excel.DirectExcelDbPump;
-import org.junit.Test;
-
-import java.io.File;
-
-/**
- * @author lixin.tian@renren-inc.com
- * @date 2018/9/20 13:24
- */
-public class DirectExcelDbPumpTest {
-
-    private static final String url = "jdbc:mysql://localhost:3306/springboot_demo?characterEncoding=utf8&zeroDateTimeBehavior=convertToNull";
-    private static final String user = "root";
-    private static final String password = "root";
-
-    @Test
-    public void test() throws Exception {
-        File file = new File("D:\\data\\车商资本金明细.xls");
-        DirectExcelDbPump pump = new DirectExcelDbPump(url, user, password);
-        // 设置文件对象
-        pump.setFile(file);
-        // 设置映射类
-        pump.setDbTableName("tb_money_detail");
-        // 设置起始行号
-        pump.setHeadRowNum(1);
-        // 设置插入模式
-        pump.setInMode(DbInMode.DELETE_AND_ADD);
-        int i = pump.execute();
-        System.out.println(i);
-    }
-
-}
+//package com.renren.jinkong.kylin.dbtool;
+//
+//import com.renren.jinkong.kylin.dbtool.core.op.DbInMode;
+//import com.renren.jinkong.kylin.dbtool.excel.DirectExcelDbPump;
+//import org.junit.Test;
+//
+//import java.io.File;
+//
+///**
+// * @author lixin.tian@renren-inc.com
+// * @date 2018/9/20 13:24
+// */
+//public class DirectExcelDbPumpTest {
+//
+//    private static final String url = "jdbc:mysql://localhost:3306/springboot_demo?characterEncoding=utf8&zeroDateTimeBehavior=convertToNull";
+//    private static final String user = "root";
+//    private static final String password = "root";
+//
+//    @Test
+//    public void test() throws Exception {
+//        File file = new File("D:\\data\\车商资本金明细.xls");
+//        DirectExcelDbPump pump = new DirectExcelDbPump(url, user, password);
+//        // 设置文件对象
+//        pump.setFile(file);
+//        // 设置映射类
+//        pump.setDbTableName("tb_money_detail");
+//        // 设置起始行号
+//        pump.setHeadRowNum(1);
+//        // 设置插入模式
+//        pump.setInMode(DbInMode.DELETE_AND_ADD);
+//        int i = pump.execute();
+//        System.out.println(i);
+//    }
+//
+//}
--- src/test/java/com/renren/jinkong/kylin/dbtool/ExcelUtilTest.java
+++ src/test/java/com/renren/jinkong/kylin/dbtool/ExcelUtilTest.java
@@ -1,32 +1,32 @@
-package com.renren.jinkong.kylin.dbtool;
-
-import com.renren.jinkong.kylin.dbtool.util.ExcelUtil;
-import org.junit.Test;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-
-/**
- * @author lixin.tian@renren-inc.com
- * @date 2018/9/25 17:05
- */
-public class ExcelUtilTest {
-
-    @Test
-    public void getAllSheetNameTest() throws IOException {
-        File file = new File("D:\\data\\各城市再投入明细修改版2.xls");
-
-        List<String> allSheetName = ExcelUtil.getAllSheetName(file);
-        System.out.println(allSheetName);
-    }
-
-    @Test
-    public void getAllSheetNameIndexMapTest() throws IOException {
-        File file = new File("D:\\data\\各城市再投入明细修改版2.xls");
-
-        Map<String, Integer> allSheetName = ExcelUtil.getAllSheetNameIndexMap(file);
-        System.out.println(allSheetName);
-    }
-}
+//package com.renren.jinkong.kylin.dbtool;
+//
+//import com.renren.jinkong.kylin.dbtool.util.ExcelUtil;
+//import org.junit.Test;
+//
+//import java.io.File;
+//import java.io.IOException;
+//import java.util.List;
+//import java.util.Map;
+//
+///**
+// * @author lixin.tian@renren-inc.com
+// * @date 2018/9/25 17:05
+// */
+//public class ExcelUtilTest {
+//
+//    @Test
+//    public void getAllSheetNameTest() throws IOException {
+//        File file = new File("D:\\data\\各城市再投入明细修改版2.xls");
+//
+//        List<String> allSheetName = ExcelUtil.getAllSheetName(file);
+//        System.out.println(allSheetName);
+//    }
+//
+//    @Test
+//    public void getAllSheetNameIndexMapTest() throws IOException {
+//        File file = new File("D:\\data\\各城市再投入明细修改版2.xls");
+//
+//        Map<String, Integer> allSheetName = ExcelUtil.getAllSheetNameIndexMap(file);
+//        System.out.println(allSheetName);
+//    }
+//}
